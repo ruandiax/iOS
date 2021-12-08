750f66624fb2b997404d4cc1e96c954e11ea48ed
Ruan Dias <75426626+ruandiax@users.noreply.github.com>
2021-12-08 05:38:32 -0300

    Add files via upload

---
+++ Rich/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ruan Dias on 08/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
